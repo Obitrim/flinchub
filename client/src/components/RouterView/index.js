1d5fc33dc26d81b33cb8799342ea3b4548d4802a
obitrim <pevangelist516@gmail.com>
2021-01-26 23:23:07 +0000

    added route views

--- client/src/components/RouterView/index.js
+++ client/src/components/RouterView/index.js
@@ -5,11 +5,19 @@ import './RouterView.css';
 
 // view components
 import Home from '../../views/Home';
+import Team from '../../views/Team';
+import ContactUs from '../../views/Contact';
+import Projects from '../../views/Projects';
+import Services from '../../views/Services';
 
 const Index = (props) => {
   return (
     <main className="RouterView">
     	<Route path="/" exact component={Home}/>
+    	<Route path="/team" component={Team}/>
+    	<Route path="/projects" component={Projects}/>
+    	<Route path="/services" component={Services}/>
+    	<Route path="/contact-us" component={ContactUs}/>
     </main>
   )
 }
