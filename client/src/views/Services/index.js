861e244030b922fc45d2e4e86177158b663ed926
obitrim <pevangelist516@gmail.com>
2021-01-26 23:22:28 +0000

    created view files for top level menu

---
+++ client/src/views/Services/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Index = (props) => {
+  return (
+    <h1>Services</h1>
+  )
+}
+
+export default Index;
---
+++ client/src/views/Team/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Index = (props) => {
+  return (
+    <h1>Team</h1>
+  )
+}
+
+export default Index;
