b23a68006ce5ef907ad0423bd0d2c31e1d7a41aa
obitrim <pevangelist516@gmail.com>
2021-01-26 21:43:34 +0000

    validating component props

--- client/src/components/Container/index.js
+++ client/src/components/Container/index.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import './Container.css';
+import PropTypes from 'prop-types';
 
 const Index = ({children, className}) => {
   return (
     <div className={`Container ${className}`}>{children}</div>
   )
+}
+
+Index.propTypes = {
+	children: PropTypes.node,
+	className: PropTypes.string
+}
+
+Index.defaultProps = {
+	children: 'Content goes here'
 }
 
 export default Index;
