36b7cfb330208e2dbf8de41add88831d404dd4a6
obitrim <pevangelist516@gmail.com>
2021-02-01 02:05:31 +0000

    modified markup

--- client/src/components/Banner/index.js
+++ client/src/components/Banner/index.js
@@ -5,9 +5,9 @@ import BaseButton from '../BaseButton';
 const Index = ({ heading, description }) => {
   return (
     <div className="Banner">
-    	<h1>{heading}</h1>
+    	<h1 className="Banner__Heading">{heading}</h1>
     	<p className="Banner__Desc">{description}</p>
-    	<BaseButton text="Hire Us"/>
+    	<BaseButton text="Hire Us" size="large"/>
     </div>
   )
 }
