fabf2d23b68d2749caca59b1db20b4fb1cf701ab
obitrim <pevangelist516@gmail.com>
2021-02-01 02:06:35 +0000

    added prop validation

--- client/src/components/BaseButton/index.js
+++ client/src/components/BaseButton/index.js
@@ -1,12 +1,31 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
 import './BaseButton.css';
 
-const Index = ({ text, onClick }) => {
+const Index = ({ text, onClick, size }) => {
   return (
-    <button type="button" className="BaseButton" onClick={onClick}>
+    <button 
+    	type="button" 
+    	className={`BaseButton ${size.toLowerCase() === 'large' && 'BaseButton--Large'}`} 
+    	onClick={onClick}>
     	{text || 'Button'}
     </button>
   )
+}
+
+Index.propTypes = {
+	text: PropTypes.string,
+	onClick: PropTypes.func,
+	size: PropTypes.oneOf(['large', 'small'])
+}
+
+Index.defaultProps = {
+	text: 'Button',
+	onClick: () => {
+		alert('Please pass onClick prop to handle click event');
+	},
+	size: 'small'
 }
 
 export default Index;
