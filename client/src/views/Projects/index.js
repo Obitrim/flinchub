7f074c7b99cdda86676b9a6ecee78698cebfc358
obitrim <pevangelist516@gmail.com>
2021-03-08 12:26:57 +0000

    refactored markup

--- client/src/views/Projects/index.js
+++ client/src/views/Projects/index.js
@@ -1,15 +1,29 @@
 import React from 'react';
 import Banner from '../../components/Banner';
+import FilterBar from '../../components/FilterBar';
+import Container from '../../components/Container';
 
 const Index = (props) => {
-  return (
-    <>
-    	<Banner 
-    		heading="Our Projects" 
-    		description="We built and satisfied our customers with our projects"
-    	/>
-    	<h1>Projects</h1>
-    </>
+	function onFilterChanged(filter){
+		console.log('current filter: ' + filter);
+	}
+
+	return (
+		<>
+			<Banner 
+				heading="Our Projects" 
+				description="We built and satisfied our customers with our projects"
+			/>
+			<Container style={{ padding: '5em 0' }}>
+				<h1 className="ViewHeading">Projects</h1>
+				{/*Filter bar*/}
+				<FilterBar 
+					onFilterChanged={onFilterChanged}
+					filters={['All', 'Websites', 'Graphic Design', 'Laptop Stickers']}
+				/>
+				{/*Filtered works*/}
+			</Container>
+		</>
   )
 }
 
