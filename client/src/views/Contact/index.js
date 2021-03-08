1b2f86dc7570bddb1868f95e08182c1741ad31e5
obitrim <pevangelist516@gmail.com>
2021-03-08 09:39:08 +0000

    refactored contact us page

--- client/src/views/Contact/index.js
+++ client/src/views/Contact/index.js
@@ -16,17 +16,17 @@ const Index = (props) => {
 	    	<ContactForm wrapperClass="Contact__Col"/>
 	    	<div className="Contact__Col Contact__Col--Right">
 	    		<div className="Contact__Address">
-		    		<h2>Address</h2>
+		    		<h2 className="Contact__Col__Heading">Address</h2>
 		    		<span>Main Branch: Kumasi - remote</span>
-		    		<h2>Phone</h2>
+		    		<h2 className="Contact__Col__Heading">Phone</h2>
 		    		<a href="tel:+233549569486">+233 549 569 486</a>
-		    		<h2>Email</h2>
+		    		<h2 className="Contact__Col__Heading">Email</h2>
 		    		<a href="mailto:info.flinchub@gmail.com">info.flinchub@gmail</a>
 		    	</div>
 		    	<div className="Contact__Info">
-		    		<h2>More Information</h2>
-		    		<p>Lorem ipsum dolor sit amet consectetur, adipisicing, elit. Ducimus molestias, blanditiis enim maxime nulla error, voluptatibus rerum id reiciendis! Cum.</p>
-		    		<p>Lorem ipsum dolor sit amet consectetur, adipisicing, elit. Ducimus molestias, blanditiis enim maxime nulla error, voluptatibus rerum id reiciendis! Cum.</p>
+		    		<h2 className="Contact__Col__Heading">More Information</h2>
+		    		<p className="Contact__Info__Text">Lorem ipsum dolor sit amet consectetur, adipisicing, elit. Ducimus molestias, blanditiis enim maxime nulla error, voluptatibus rerum id reiciendis! Cum.</p>
+		    		<p className="Contact__Info__Text">Lorem ipsum dolor sit amet consectetur, adipisicing, elit. Ducimus molestias, blanditiis enim maxime nulla error, voluptatibus rerum id reiciendis! Cum.</p>
 		    	</div>
 	    	</div>
     	</Container>
