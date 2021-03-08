395746cf446dc2ae402de584330a35b411c2b370
obitrim <pevangelist516@gmail.com>
2021-03-08 09:41:03 +0000

    refactored markup

--- client/src/views/Team/index.js
+++ client/src/views/Team/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
+
+import './Team.css';
 import Banner from '../../components/Banner';
+import Container from '../../components/Container';
 
 const Index = (props) => {
   return (
@@ -8,7 +11,19 @@ const Index = (props) => {
     		heading="Our Team" 
     		description="We are a team of enthusiastic and effecient professionals hoping to help promote businesses with our services"
     	/>
-    	<h1>Team</h1>
+    	{/*Information about entire team*/}
+    	<Container>
+    		<div className="Team__GeneralInfo">
+    			<h1>Our Team</h1>
+    			<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam placeat accusamus error reiciendis tenetur deleniti dolorem tempora reprehenderit voluptas, quaerat, in architecto quam blanditiis. Esse consequatur similique dicta, aliquam excepturi?</p>
+    			<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam placeat accusamus error reiciendis tenetur deleniti dolorem tempora reprehenderit voluptas, quaerat, in architecto quam blanditiis. Esse consequatur similique dicta, aliquam excepturi?</p>
+    			<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam placeat accusamus error reiciendis tenetur deleniti dolorem tempora reprehenderit voluptas, quaerat, in architecto quam blanditiis. Esse consequatur similique dicta, aliquam excepturi?</p>
+    		</div>
+    	</Container>
+    	{/*Team member cards*/}
+    	{/*Team member cards*/}
+    	{/*Team member cards*/}
+    	{/*Team member cards*/}
     </>
   )
 }
