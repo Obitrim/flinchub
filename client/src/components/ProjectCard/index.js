85267d89cafb71f22d3815ab30d409d82baac5ec
obitrim <pevangelist516@gmail.com>
2021-03-10 11:55:49 +0000

    added prop validation and refactored markup

--- client/src/components/ProjectCard/index.js
+++ client/src/components/ProjectCard/index.js
@@ -1,13 +1,29 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const Index = ({title, image, previewUrl, ...props}) => {
+import './ProjectCard.css';
+
+const Index = ({title, image, previewUrl, shadow, ...props}) => {
   return (
-    <div className="ProjectCard" {...props}>
-    	<h3 className="ProjectCard__Heading">{title}</h3>
-    	{!!previewUrl && <a href={previewUrl}>Preview</a>}
-    	<img src={image} alt={title} />
+    <div className={`ProjectCard ${ shadow && "ProjectCard--Shadow"}`} {...props}>
+    	<div className="ProjectCard__Overlay">
+    		<h3 className="ProjectCard__Heading">{title}</h3>
+    		{!!previewUrl && <a className="ProjectCard__Link" href={previewUrl}>Preview</a>}
+    	</div>
+    	<img className="ProjectCard__Img" src={image} alt={title} />
     </div>
   )
+}
+
+Index.propTypes = {
+	title: PropTypes.string,
+	image: PropTypes.any,
+	previewUrl: PropTypes.string,
+	shadow: PropTypes.bool
+}
+
+Index.defaultProps = {
+	shadow: false
 }
 
 export default Index;
