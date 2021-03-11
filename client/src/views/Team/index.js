ca500c88e622428230cc9e68404ffb2d1b11bcd8
obitrim <pevangelist516@gmail.com>
2021-03-11 22:01:11 +0000

    refactored template

--- client/src/views/Team/index.js
+++ client/src/views/Team/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import './Team.css';
 import Banner from '../../components/Banner';
+import TeamCard from '../../components/TeamCard';
 import Container from '../../components/Container';
 
 const Index = (props) => {
@@ -12,18 +13,25 @@ const Index = (props) => {
     		description="We are a team of enthusiastic and effecient professionals hoping to help promote businesses with our services"
     	/>
     	{/*Information about entire team*/}
-    	<Container>
-    		<div className="Team__GeneralInfo">
-    			<h1>Our Team</h1>
-    			<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam placeat accusamus error reiciendis tenetur deleniti dolorem tempora reprehenderit voluptas, quaerat, in architecto quam blanditiis. Esse consequatur similique dicta, aliquam excepturi?</p>
-    			<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam placeat accusamus error reiciendis tenetur deleniti dolorem tempora reprehenderit voluptas, quaerat, in architecto quam blanditiis. Esse consequatur similique dicta, aliquam excepturi?</p>
-    			<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam placeat accusamus error reiciendis tenetur deleniti dolorem tempora reprehenderit voluptas, quaerat, in architecto quam blanditiis. Esse consequatur similique dicta, aliquam excepturi?</p>
-    		</div>
+    	<Container className="Team__Container">
+    		<section className="Team__GeneralInfo">
+    			<h1 className="ViewHeading">Our Team</h1>
+    			<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam placeat accusamus error reiciendis tenetur deleniti dolorem tempora reprehender</p>
+    			<p>Lorem ipsum dolor sit amet consectetur adipisihenderit voluptas, quaerat, in architecto quam blanditiis. Esse consequatur similique dicta, aliquam excepturi?</p>
+    			<p>Lorem lorem tempora reprehenderit voluptas, quaerat, in architecto quam blanditiis. Esse consequatur similique dicta, aliquam excepturi?</p>
+    		</section>
+            <section className="Team__Members">
+                <TeamCard/>
+                <TeamCard/>
+                <TeamCard/>
+                <TeamCard/>
+                <TeamCard/>
+                <TeamCard/>
+                <TeamCard/>
+                <TeamCard/>
+            </section>
     	</Container>
-    	{/*Team member cards*/}
-    	{/*Team member cards*/}
-    	{/*Team member cards*/}
-    	{/*Team member cards*/}
+    	
     </>
   )
 }
